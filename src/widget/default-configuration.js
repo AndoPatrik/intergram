854b8bf98442a78df2596291564a374cf929ac59
Mate Molnar <matemolnar88@gmail.com>
2021-06-21 23:13:48 +0200

    style update 3

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -6,7 +6,7 @@ export const defaultConfiguration = {
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
     introMessage: 'Üdvözöljük! Miben segíthetünk?',
-    autoResponse: 'Szíves türelmét kérjük, legközelebbi kollégánk rövidesen válaszol!',
+    autoResponse: 'Szíves türelmét kérjük, kollégánk rövidesen írni fog Önnek!',
     autoNoResponse: 'Sajnos úgy tűnik egyik kollégánk sem elérhető pillanatnyilag. Kérjük adja meg az email címét, hogy fel tudjuk venni Önnel a kapcsolatot!',
     placeholderText: 'Üzenet küldése...',
     displayMessageTime: true,
